ce68f49f0aabbbbc8355a08024166481c229dcae
cherfaneranda <randacherfane@gmail.com>
2014-03-20 21:50:23 +0200

    .

--- angularJs-Demo/app/js/app.js
+++ angularJs-Demo/app/js/app.js
@@ -12,19 +12,7 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  //$routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
   
-  $routeProvider.when('/view3/', {
-    controller:'ListCtrl',
-    templateUrl:'partials/list.html'
-    });
-    $routeProvider.when('/view3/edit/:projectId', {
-    controller:'EditCtrl',
-    templateUrl:'partials/detail.html'
-    });
-    $routeProvider.when('/view3/new', {
-    controller:'CreateCtrl',
-    templateUrl:'partials/detail.html'
-    });
-    $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
+
